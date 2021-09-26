f6d1e033d304cff24b242c2a4f7dbafd8f1ac75c
dennis9707 <he0707@hotmail.com>
2021-09-26 15:37:40 +0800

    fix: edit calling functions

--- split/main/main.go
+++ split/main/main.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/eroatta/token/conserv"
+	"github.com/eroatta/token/expansion"
+	"github.com/eroatta/token/gentest"
+	"github.com/eroatta/token/greedy"
+	"github.com/eroatta/token/lists"
 )
+func run_gentest() {
+	var simCalculator gentest.SimilarityCalculator
+	context := lists.NewBuilder().Add(lists.Dictionary.Elements()...).
+		Add(lists.KnownAbbreviations.Elements()...).
+		Add(lists.Stop.Elements()...).
+		Build()
+	possibleExpansions := expansion.NewSetBuilder().AddList(lists.Dictionary).Build()
 
-func main() {
+	splitted := gentest.Split("httpResponse", simCalculator, context, possibleExpansions)
+
+	fmt.Println(splitted) // [http response]
+}
+
+func run_greedy() {
+	list := lists.NewBuilder().Add(lists.Dictionary.Elements()...).
+		Add(lists.KnownAbbreviations.Elements()...).
+		Add(lists.Stop.Elements()...).
+		Build()
+	splitted := greedy.Split("httpResponse", list)
+
+	fmt.Println(splitted) // "http response"
+}
+
+func run_conserv() {
 	splitted := conserv.Split("httpResponse")
 
 	fmt.Println(splitted) // "http response"
+}
+
+func main() {
+	fmt.Println("gentest")
+	run_gentest()
+	fmt.Println("greedy")
+	run_greedy()
+	fmt.Println("conserv")
+	run_conserv()
 }
